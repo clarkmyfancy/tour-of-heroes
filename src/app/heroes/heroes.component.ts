68f806edc080a9c0666d505865df9f0284398b43
clarkmyfancy <clark.jack0@gmail.com>
2020-01-13 13:53:53 -0600

    added HeroService and changed #getHeroes to have a async signature

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -10,16 +12,25 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService
+  ) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    let observableHeroes = this.heroService.getHeroes();
+    observableHeroes.subscribe(recievedHeroes => this.heroes = recievedHeroes);
+    // this.heroes = this.heroService.getHeroes();
   }
 
 }
