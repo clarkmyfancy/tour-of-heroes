1ea4d1dbf37a3fec185928e1051224dd154b824e
clarkmyfancy <clark.jack0@gmail.com>
2020-01-14 12:55:52 -0600

    added http requests for adding, updating, getting and deleting heroes, also refactored

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,7 +5,6 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
@@ -16,52 +15,83 @@ export class HeroService {
   // url to web api
   private heroesUrl = 'api/heroes';
 
+  httpOptions = {
+    headers: new HttpHeaders(
+      { 'Content-Type': 'application/json' }
+    )
+  };
+
   constructor(
     private messageService: MessageService,
     private http: HttpClient
   ) { }
 
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero with id: ${id}`)),
+      catchError(this.handleFailedHttpOperation<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('Fetched heroes')),
+      catchError(this.handleFailedHttpOperation<Hero[]>('getHeroes', []))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`Updated hero with id: ${hero.id}`)),
+      catchError(this.handleFailedHttpOperation<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`Adding new hero with name: ${newHero.name} and id: ${newHero.id}`)),
+      catchError(this.handleFailedHttpOperation<Hero>('addHero'))
+    );
+  }
+
+  removeHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`Deleted hero with id: ${id}`)),
+      catchError(this.handleFailedHttpOperation<Hero>('removeHero'))
+    );
+  }
+
+  searchForHeroesWithTerm(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array
+      return of([]);
+    }
+
+    const url = `${this.heroesUrl}/?name=${term}`;
+    return this.http.get<Hero[]>(url).pipe(
+      tap(_ => this.log(`Found heroes matching ${term}`)),
+      catchError(this.handleFailedHttpOperation<Hero[]>('searchForHeroesWithTerm', []))
+    );
+  }
+
   private log(message: string) {
     this.messageService.addMessage(`HeroService: ${message}`);
   }
 
-  getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.log(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
-  }
-
-  // getHeroes(): Observable<Hero[]> {
-  //   // TODO: send the message _after_ fetching the heroes
-  //   this.log('HeroService: fetched heroes');
-  //   return of(HEROES);
-  // }
-
-  // get heroes from the server
-  getHeroes(): Observable<Hero[]> {
-    this.log('HeroService: fetching heroes');
-
-    let observable = this.http.get<Hero[]>(this.heroesUrl);
-
-    // function takes the name of the operation that failed and a safe return value
-    observable.pipe(
-      catchError(this.handleFailedHttpOperation<Hero[]>('getHeroes', []))
-    );
-    return observable;
-  }
-
-  private handleFailedHttpOperation<T>(operation = 'operation', result?: T) {
+  private handleFailedHttpOperation<T>(operationThatFailed = 'operation', safeReturnValue?: T) {
     return (error: any): Observable<T> => {
 
       // TODO: send the error to remote logging infrastructure
       console.error(error);
 
       // TODO: better job of transofming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
+      this.log(`${operationThatFailed} failed: ${error.message}`);
 
       // Let the app keep running by returning empty result
-      return of(result as T);
+      return of(safeReturnValue as T);
     }
   }
-
 }
